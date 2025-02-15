52a3f755a901e168e50f60d84220d663f833cabf
Chris <chris.tian@biposervice.com>
2025-02-15 18:43:35 +0800

    feat: structured

---
+++ demo_06.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from pydantic import BaseModel, Field
+
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    name: Optional[str] = Field(default=None, description="The name of the person")
+    hair_color: Optional[str] = Field(
+        default=None, description="The color of the person's hair if known"
+    )
+    height_in_meters: Optional[str] = Field(
+        default=None, description="Height measured in meters"
+    )
+
+# Define a custom prompt to provide instructions and any additional context.
+# 1) You can add examples into the prompt template to improve extraction quality
+# 2) Introduce additional parameters to take context into account (e.g., include metadata
+#    about the document from which the text was extracted.)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert extraction algorithm. "
+            "Only extract relevant information from the text. "
+            "If you do not know the value of an attribute asked to extract, "
+            "return null for the attribute's value.",
+        ),
+        # Please see the how-to about improving performance with
+        # reference examples.
+        # MessagesPlaceholder('examples'),
+        ("human", "{text}"),
+    ]
+)
+
+
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(model='llama3.1:8b')
+
+structured_llm = llm.with_structured_output(schema=Person)
+
+
+text = "Alan Smith is 6 feet tall and has blond hair."
+prompt = prompt_template.invoke({"text": text})
+response = structured_llm.invoke(prompt)
+
+print(response)
