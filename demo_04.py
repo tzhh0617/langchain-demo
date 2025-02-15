9802489477e5c5552c4633b4413f97bbd44dc433
Chris <chris.tian@biposervice.com>
2025-02-15 17:36:37 +0800

    feat: splitter

---
+++ demo_04.py
@@ -0,0 +1,20 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+file_path = "./example_data/nke-10k-2023.pdf"
+loader = PyPDFLoader(file_path)
+
+docs = loader.load()
+
+print(len(docs))
+# print(f"{docs[0].page_content[:200]}\n")
+# print(docs[0].metadata)
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(len(all_splits))
---
+++ demo_05.py
@@ -0,0 +1,8 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
+
+len(all_splits)
