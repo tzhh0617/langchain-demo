1b16e397ac11fbc2fdbe8a16624ebbd3def1ce86
Chris <chris.tian@biposervice.com>
2025-02-15 18:29:57 +0800

    feat: classification

--- demo_05.py
+++ demo_05.py
@@ -1,8 +1,34 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel, Field
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
+tagging_prompt = ChatPromptTemplate.from_template(
+    """
+Extract the desired information from the following passage.
+
+Only extract the properties mentioned in the 'Classification' function.
+
+Passage:
+{input}
+"""
 )
-all_splits = text_splitter.split_documents(docs)
 
-len(all_splits)
+
+class Classification(BaseModel):
+    sentiment: str = Field(description="The sentiment of the text")
+    aggressiveness: int = Field(
+        description="How aggressive the text is on a scale from 1 to 10"
+    )
+    language: str = Field(description="The language the text is written in")
+
+
+# LLM
+llm = ChatOllama( model = "llama3.1").with_structured_output(
+    Classification
+)
+
+inp = "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!"
+prompt = tagging_prompt.invoke({"input": inp})
+response = llm.invoke(prompt)
+
+print(response)
