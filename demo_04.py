d10b0bd9ca577c241dc3f1c194ab5faa8c5b6c33
Chris <chris.tian@biposervice.com>
2025-02-15 18:19:47 +0800

    chore

--- demo_04.py
+++ demo_04.py
@@ -5,7 +5,7 @@ loader = PyPDFLoader(file_path)
 
 docs = loader.load()
 
-print(len(docs))
+# print(len(docs))
 # print(f"{docs[0].page_content[:200]}\n")
 # print(docs[0].metadata)
 
@@ -17,4 +17,43 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 all_splits = text_splitter.split_documents(docs)
 
-print(len(all_splits))
+# print(len(all_splits))
+
+
+from langchain_ollama import OllamaEmbeddings
+embeddings = OllamaEmbeddings( model = "llama3.1")
+
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+# assert len(vector_1) == len(vector_2)
+# print(f"Generated vectors of length {len(vector_1)}\n")
+# print(vector_1[:10])
+
+
+
+from langchain_core.vectorstores import InMemoryVectorStore
+
+vector_store = InMemoryVectorStore(embeddings)
+ids = vector_store.add_documents(documents=all_splits)
+
+
+# results = vector_store.similarity_search(
+#     "How many distribution centers does Nike have in the US?"
+# )
+# print(results[0])
+
+
+# Note that providers implement different scores; the score here
+# is a distance metric that varies inversely with similarity.
+
+# results = vector_store.similarity_search_with_score("What was Nike's revenue in 2023?")
+# doc, score = results[0]
+# print(f"Score: {score}\n")
+# print(doc)
+
+
+embedding = embeddings.embed_query("How were Nike's margins impacted in 2023?")
+
+results = vector_store.similarity_search_by_vector(embedding)
+print(results[0])
